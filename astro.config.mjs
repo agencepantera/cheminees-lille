e434df8520d78bda71cc38ba2c4fcbfad22eaf48
agencepantera <agencepantera@gmail.com>
2024-12-11 15:47:06 +0100

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://cheminees-lille.fr',
   integrations: [
     tailwind(),
-    mdx(),
-    sitemap(),
-  ],
+    sitemap()
+  ]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+// Déclaration de type pour AOS
+declare const AOS: {
+  init: (options?: {
+    duration?: number;
+    once?: boolean;
+    // Ajoutez d'autres options selon vos besoins
+  }) => void;
+};
